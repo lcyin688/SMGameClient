fe3aa6598596ff8e0419fa3f0666f07928314dd4
lcyin688 <40627992+lcyin688@users.noreply.github.com>
2024-07-04 23:15:49 +0800

    消灭心心

--- assets/entrance/script/GameLoading/GameLoadingView.ts
+++ assets/entrance/script/GameLoading/GameLoadingView.ts
@@ -14,17 +14,12 @@ export default class GameLoadingView extends UIViewBase {
     public barLoading: cc.Node;
     public barLoadingSprite: cc.Sprite = undefined;
     public barLoadingProgressBar: cc.ProgressBar = undefined;
-    public noticeTop: cc.Node;
-    public noticeTopLabel: cc.Label = undefined;
-    public noticeTopLabelOutline: cc.LabelOutline = undefined;
     public noticeBot: cc.Node;
     public noticeBotLabel: cc.Label = undefined;
     public noticeBotLabelOutline: cc.LabelOutline = undefined;
     public progress: cc.Node;
     public progressLabel: cc.Label = undefined;
     public progressLabelOutline: cc.LabelOutline = undefined;
-    public loadIcon: cc.Node;
-    public loadIconSprite: cc.Sprite = undefined;
     
 
     public onLoad() {
@@ -50,17 +45,12 @@ export default class GameLoadingView extends UIViewBase {
         this.barLoading = this.get('_barLoading_');
         this.barLoadingSprite = this.barLoading.getComponent(cc.Sprite);
         this.barLoadingProgressBar = this.barLoading.getComponent(cc.ProgressBar);
-        this.noticeTop = this.get('_noticeTop_');
-        this.noticeTopLabel = this.noticeTop.getComponent(cc.Label);
-        this.noticeTopLabelOutline = this.noticeTop.getComponent(cc.LabelOutline);
         this.noticeBot = this.get('_noticeBot_');
         this.noticeBotLabel = this.noticeBot.getComponent(cc.Label);
         this.noticeBotLabelOutline = this.noticeBot.getComponent(cc.LabelOutline);
         this.progress = this.get('_progress_');
         this.progressLabel = this.progress.getComponent(cc.Label);
         this.progressLabelOutline = this.progress.getComponent(cc.LabelOutline);
-        this.loadIcon = this.get('_loadIcon_');
-        this.loadIconSprite = this.loadIcon.getComponent(cc.Sprite);
         
     }
 
